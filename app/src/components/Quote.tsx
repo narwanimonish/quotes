29439f07a9e76065e76d2c51fc472324165ff1a7
Monish Narwani <narwanimonish@gmail.com>
2022-07-03 22:57:47 +0530

    Small font size change for mobile

--- app/src/components/Quote.tsx
+++ app/src/components/Quote.tsx
@@ -8,7 +8,7 @@ export default function Quote({ quote }: { quote: string[] }) {
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             {/* <div className="sm:text-center lg:text-left"> */}
             <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-8xl md:text-6xl">
                 <span className="block xl:inline">{quote[0]}</span>
                 <span className="block text-indigo-600 xl:inline">
                   {quote[1]}
